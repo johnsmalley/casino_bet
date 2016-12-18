0184385dd1b811a14cd4c000c8f553163eb7abe4
johnsmalley <john.m.smalley@gmail.com>
2016-12-18 18:56:34 -0500

    created a deck shuffler

--- blackjack-spec.js
+++ blackjack-spec.js
@@ -1,3 +1,4 @@
+/* global describe it */
 var expect = require('chai').expect
 
 describe('Blackjack', function () {
@@ -6,26 +7,227 @@ describe('Blackjack', function () {
     expect(Blackjack).to.not.be.undefined
   })
 
-  describe('Sorted Deck', function() {
-    it('Sorted deck should have 52 cards', function() {
-      var SORTED_DECK = require('./blackjack.js').SORTED_DECK
-      expect(SORTED_DECK.length).to.equal(52)
-    })
+  describe('Sorted Deck', function () {
+    it('Sorted deck should contain 1 deck', function () {
+      var createDecks = require('./blackjack.js').createDecks
+      var generatedDeck = createDecks(1)
+      var viewedDeck = {}
+      const COUNT = 1
+      var expectDeck = {
+        '2C': COUNT,
+        '3C': COUNT,
+        '4C': COUNT,
+        '5C': COUNT,
+        '6C': COUNT,
+        '7C': COUNT,
+        '8C': COUNT,
+        '9C': COUNT,
+        '10C': COUNT,
+        'JC': COUNT,
+        'QC': COUNT,
+        'KC': COUNT,
+        'AC': COUNT,
+        '2S': COUNT,
+        '3S': COUNT,
+        '4S': COUNT,
+        '5S': COUNT,
+        '6S': COUNT,
+        '7S': COUNT,
+        '8S': COUNT,
+        '9S': COUNT,
+        '10S': COUNT,
+        'JS': COUNT,
+        'QS': COUNT,
+        'KS': COUNT,
+        'AS': COUNT,
+        '2D': COUNT,
+        '3D': COUNT,
+        '4D': COUNT,
+        '5D': COUNT,
+        '6D': COUNT,
+        '7D': COUNT,
+        '8D': COUNT,
+        '9D': COUNT,
+        '10D': COUNT,
+        'JD': COUNT,
+        'QD': COUNT,
+        'KD': COUNT,
+        'AD': COUNT,
+        '2H': COUNT,
+        '3H': COUNT,
+        '4H': COUNT,
+        '5H': COUNT,
+        '6H': COUNT,
+        '7H': COUNT,
+        '8H': COUNT,
+        '9H': COUNT,
+        '10H': COUNT,
+        'JH': COUNT,
+        'QH': COUNT,
+        'KH': COUNT,
+        'AH': COUNT
+      }
 
-    it('Sorted deck should have 4 suits', function() {
-      var SORTED_DECK = require('./blackjack.js').SORTED_DECK
-      var suitHolder = {}
-      SORTED_DECK.forEach(function(card) {
-        if (!suitHolder[card[card.length - 1]]) {
-          suitHolder[card[card.length - 1]] = true
+      generatedDeck.forEach(function (card) {
+        if (viewedDeck[card]) {
+          viewedDeck[card]++
+        } else {
+          viewedDeck[card] = 1
         }
       })
-      expect(suitHolder).to.deep.equal({
-        C: true,
-        S: true,
-        H: true,
-        D: true
+
+      expect(viewedDeck).to.deep.equal(expectDeck)
+    })
+
+    it('Sorted deck should contain 5 deck', function () {
+      var createDecks = require('./blackjack.js').createDecks
+      var generatedDeck = createDecks(5)
+      var viewedDeck = {}
+      const COUNT = 5
+      var expectDeck = {
+        '2C': COUNT,
+        '3C': COUNT,
+        '4C': COUNT,
+        '5C': COUNT,
+        '6C': COUNT,
+        '7C': COUNT,
+        '8C': COUNT,
+        '9C': COUNT,
+        '10C': COUNT,
+        'JC': COUNT,
+        'QC': COUNT,
+        'KC': COUNT,
+        'AC': COUNT,
+        '2S': COUNT,
+        '3S': COUNT,
+        '4S': COUNT,
+        '5S': COUNT,
+        '6S': COUNT,
+        '7S': COUNT,
+        '8S': COUNT,
+        '9S': COUNT,
+        '10S': COUNT,
+        'JS': COUNT,
+        'QS': COUNT,
+        'KS': COUNT,
+        'AS': COUNT,
+        '2D': COUNT,
+        '3D': COUNT,
+        '4D': COUNT,
+        '5D': COUNT,
+        '6D': COUNT,
+        '7D': COUNT,
+        '8D': COUNT,
+        '9D': COUNT,
+        '10D': COUNT,
+        'JD': COUNT,
+        'QD': COUNT,
+        'KD': COUNT,
+        'AD': COUNT,
+        '2H': COUNT,
+        '3H': COUNT,
+        '4H': COUNT,
+        '5H': COUNT,
+        '6H': COUNT,
+        '7H': COUNT,
+        '8H': COUNT,
+        '9H': COUNT,
+        '10H': COUNT,
+        'JH': COUNT,
+        'QH': COUNT,
+        'KH': COUNT,
+        'AH': COUNT
+      }
+
+      generatedDeck.forEach(function (card) {
+        if (viewedDeck[card]) {
+          viewedDeck[card]++
+        } else {
+          viewedDeck[card] = 1
+        }
       })
+
+      expect(viewedDeck).to.deep.equal(expectDeck)
+    })
+
+    it('Sorted deck should contain 6 deck', function () {
+      var createDecks = require('./blackjack.js').createDecks
+      var generatedDeck = createDecks(6)
+      var viewedDeck = {}
+      const COUNT = 6
+      var expectDeck = {
+        '2C': COUNT,
+        '3C': COUNT,
+        '4C': COUNT,
+        '5C': COUNT,
+        '6C': COUNT,
+        '7C': COUNT,
+        '8C': COUNT,
+        '9C': COUNT,
+        '10C': COUNT,
+        'JC': COUNT,
+        'QC': COUNT,
+        'KC': COUNT,
+        'AC': COUNT,
+        '2S': COUNT,
+        '3S': COUNT,
+        '4S': COUNT,
+        '5S': COUNT,
+        '6S': COUNT,
+        '7S': COUNT,
+        '8S': COUNT,
+        '9S': COUNT,
+        '10S': COUNT,
+        'JS': COUNT,
+        'QS': COUNT,
+        'KS': COUNT,
+        'AS': COUNT,
+        '2D': COUNT,
+        '3D': COUNT,
+        '4D': COUNT,
+        '5D': COUNT,
+        '6D': COUNT,
+        '7D': COUNT,
+        '8D': COUNT,
+        '9D': COUNT,
+        '10D': COUNT,
+        'JD': COUNT,
+        'QD': COUNT,
+        'KD': COUNT,
+        'AD': COUNT,
+        '2H': COUNT,
+        '3H': COUNT,
+        '4H': COUNT,
+        '5H': COUNT,
+        '6H': COUNT,
+        '7H': COUNT,
+        '8H': COUNT,
+        '9H': COUNT,
+        '10H': COUNT,
+        'JH': COUNT,
+        'QH': COUNT,
+        'KH': COUNT,
+        'AH': COUNT
+      }
+
+      generatedDeck.forEach(function (card) {
+        if (viewedDeck[card]) {
+          viewedDeck[card]++
+        } else {
+          viewedDeck[card] = 1
+        }
+      })
+
+      expect(viewedDeck).to.deep.equal(expectDeck)
+    })
+
+    it('should shuffle deck and return an array equal to input length', function () {
+      var shuffleDecks = require('./blackjack.js').shuffleDecks
+      var insertArray = [1, 2, 3, 4, 5]
+
+      var tempSuffleDecks = shuffleDecks(insertArray)
+      expect(tempSuffleDecks).to.be.a('array')
+      expect(tempSuffleDecks.length).to.equal(insertArray.length)
     })
   })
 })
--- blackjack.js
+++ blackjack.js
@@ -1,3 +1,30 @@
-const SORTED_DECK = ['2C','3C','4C','5C','6C','7C','8C','9C','10C','JC','QC','KC','AC','2S','3S','4S','5S','6S','7S','8S','9S','10S','JS','QS','KS','AS','2H','3H','4H','5H','6H','7H','8H','9H','10H','JH','QH','KH','AH','2D','3D','4D','5D','6D','7D','8D','9D','10D','JD','QD','KD','AD']
 
-exports.SORTED_DECK = SORTED_DECK
+// creates a deck(s) with an input of how many decks to use
+var createDecks = function (numOfDecks) {
+  const STANDARD_DECK = ['2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC', 'AC', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS', 'AS', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH', 'KH', 'AH', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JD', 'QD', 'KD', 'AD']
+
+  var generatedDeck = []
+
+  for (var i = 0; i < numOfDecks; i++) {
+    generatedDeck = generatedDeck.concat(STANDARD_DECK)
+  }
+
+  return generatedDeck
+}
+
+// returns a shuffled deck
+var shuffleDecks = function (decks) {
+  var decksCopy = decks.slice()
+
+  for (var i = decksCopy.length; i > 0; i--) {
+    var randomPoint = Math.floor((Math.random() * i))
+    var temp = decksCopy[randomPoint]
+    decksCopy[randomPoint] = decksCopy[i - 1]
+    decksCopy[i - 1] = temp
+  }
+
+  return decksCopy
+}
+
+exports.createDecks = createDecks
+exports.shuffleDecks = shuffleDecks
